71042807aaa857c2b2fa6c5c83a477bc9d493df7
Carl Woodward <carl@88cartell.com>
2014-09-07 21:31:02 +1000

    Color file diffs.

--- web/scripts/repo_controller.js
+++ web/scripts/repo_controller.js
@@ -5,25 +5,43 @@ RepoController.controller('RepoController', ['$scope', '$http', '$routeParams',
     var clientParams = '&client_id=c3a2cf5bc90344e47858&client_secret=650c7e530b49c3d89126d928991bf8e4eaf129d1'
 
     var transformSummary = function(commit) {
-      commit["short_sha"] = commit.sha.substring(0, 8);
-      commit["formatted_date"] = moment(commit.commit.author.date).format("DD MMM YYYY");
+      commit['short_sha'] = commit.sha.substring(0, 8);
+      commit['formatted_date'] = moment(commit.commit.author.date).format('DD MMM YYYY');
       var message = commit.commit.message;
-      commit["title"] = message.substring(0, message.indexOf("\n") == -1 ? message.length : message.indexOf("\n"));
+      commit['title'] = message.substring(0, message.indexOf('\n') == -1 ? message.length : message.indexOf('\n'));
       return commit;
     };
 
     var transformDetail = function(commit) {
-      commit["formatted_date"] = moment(commit.commit.author.date).format("DD MMM YYYY h:mm:ss a");
+      commit['formatted_date'] = moment(commit.commit.author.date).format('DD MMM YYYY h:mm:ss a');
       var message = commit.commit.message;
-      var index = message.indexOf("\n");
-      commit["title"] = message.substring(0, index == -1 ? message.length : index);
-      commit["body"] = "";
+      var index = message.indexOf('\n');
+      commit['title'] = message.substring(0, index == -1 ? message.length : index);
+      commit['body'] = '';
       if(index != -1) {
-        commit["body"] = message.substring(index + 1, message.length);
+        commit['body'] = message.substring(index + 1, message.length);
       }
+
       for(var i = 0; i < commit.files.length; i++) {
-        commit.files[i].lines = commit.files[i].patch.split('\n');
-        console.log(commit.files[i].lines);
+        var lines = commit.files[i].patch.split('\n');
+        commit.files[i].lines = [];
+        for(var j = 0; j < lines.length; j++) {
+          var kind = 'default';
+          if(lines[j].indexOf('+') === 0) {
+            kind = 'add';
+          }
+          else if(lines[j].indexOf('-') === 0) {
+            kind = 'delete';
+          }
+          else if(lines[j].indexOf('@@') === 0) {
+            kind = 'marker';
+          }
+          else {
+            kind = 'context';
+          }
+          var line = { value: lines[j], kind: kind }
+          commit.files[i].lines.push(line);
+        }
       }
       return commit;
     };
